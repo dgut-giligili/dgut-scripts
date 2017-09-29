de989470ddfb99aff25e5a433860dda25c7faa4e
Victorique Ko <victoriqueko@users.noreply.github.com>
2017-09-29 11:51:18 +0800

    Add script

---
+++ jwc/jyk.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < 15; i++) {
+  if (i in { 2: '', 3: '', 5: '', 6: '', 8: '', 10: '' })
+    document.getElementById('a' + i + '_2').checked = true;
+  else
+    document.getElementById('a' + i + '_0').checked = true;
+}
+
+fpaper.submit.click();
---
+++ unknown/score.js
@@ -0,0 +1,2 @@
+// 我忘记这段代码是干什么用的了
+document.getElementsByName('score[]').forEach(x => x.selectedIndex = x.options.length - 1);
