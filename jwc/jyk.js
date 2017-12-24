e647234632b1f6a3cd504fbbdc253c707d1cb06d
Victorique Ko <victoriqueko@users.noreply.github.com>
2017-12-25 04:34:57 +0800

    Update jyk.js

--- jwc/jyk.js
+++ jwc/jyk.js
@@ -1,4 +1,4 @@
-for (var i = 0; i < 15; i++) {
+for (var i = 1; i <= 15; i++) {
   if (i in { 2: '', 3: '', 5: '', 6: '', 8: '', 10: '' })
     document.getElementById('a' + i + '_2').checked = true;
   else
